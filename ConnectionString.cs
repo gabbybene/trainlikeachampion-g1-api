c38d7ec9d522ef6cebcb3edaa26559055da4f183
Gabby Benefield <GabbyMoody@Gabrielles-iMac.local>
2021-04-22 15:50:43 -0500

    Updating ConnectionString to JawsDB

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -2,15 +2,17 @@ namespace api
 {
     public class ConnectionString
     {
-        public string cs;
-        public ConnectionString(){
-            string server = "us-cdbr-east-03.cleardb.com";
-            string database = "heroku_000524d39fc1c75";
-            string user = "bdad55eb85c47c";
-            string password = "f1b4bad2";
+        public string cs{get; set;}
+
+        public ConnectionString()
+        {
+            string server = "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+            string database = "ikmvdl6i7su2hvr7";
             string port = "3306";
- 
-            cs = $@"server={server};user={user};database={database};port={port};password={password};convert zero datetime=True";
+            string userName = "y4t80adbo02rm95h";
+            string password = "xhzboysti8qmj02e";
+
+            cs = $@"server = {server}; user={userName}; database={database}; port={port}; password={password};";   
         }
     }
 }
