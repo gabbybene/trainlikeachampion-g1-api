51b1c364c062bcf063f85e270b34d0d01450227c
Gabby Benefield <GabbyMoody@Gabrielles-iMac.local>
2021-04-22 15:51:46 -0500

    Another ConnectionString update

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -2,17 +2,15 @@ namespace api
 {
     public class ConnectionString
     {
-        public string cs{get; set;}
-
-        public ConnectionString()
-        {
+        public string cs;
+        public ConnectionString(){
             string server = "cis9cbtgerlk68wl.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
             string database = "ikmvdl6i7su2hvr7";
-            string port = "3306";
-            string userName = "y4t80adbo02rm95h";
+            string user = "y4t80adbo02rm95h";
             string password = "xhzboysti8qmj02e";
-
-            cs = $@"server = {server}; user={userName}; database={database}; port={port}; password={password};";   
+            string port = "3306";
+ 
+            cs = $@"server={server};user={user};database={database};port={port};password={password};convert zero datetime=True";
         }
     }
 }
