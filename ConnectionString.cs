20c684286a1a9534ede1d5d849b9252c98041245
Gabby Benefield <GabbyMoody@Gabrielles-iMac.local>
2021-04-22 15:31:26 -0500

    changed ConnectionString

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -2,17 +2,15 @@ namespace api
 {
     public class ConnectionString
     {
-        public string cs{get; set;}
-
-        public ConnectionString()
-        {
+        public string cs;
+        public ConnectionString(){
             string server = "us-cdbr-east-03.cleardb.com";
             string database = "heroku_000524d39fc1c75";
             string user = "bdad55eb85c47c";
             string password = "f1b4bad2";
             string port = "3306";
-
-            cs = $@"Server={server};Uid={user};Database={database};Port={port};Pwd={password}";
+ 
+            cs = $@"server={server};user={user};database={database};port={port};password={password};convert zero datetime=True";
         }
     }
 }
